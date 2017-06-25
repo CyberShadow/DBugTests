91fdb1aa3874e217e20ce9aa9659cdc69711f33b
Vladimir Panteleev <git@thecybershadow.net>
2017-06-25 11:03:37 +0000

    random: Make non-Windows-specific

--- populate/random.d
+++ populate/random.d
@@ -1,19 +1,25 @@
-import core.sys.windows.windows;
-
 import std.algorithm.iteration;
+import std.algorithm.searching;
 import std.array;
 import std.file;
+import std.path;
 import std.process;
 import std.random;
+import std.string;
 
 import ae.utils.meta;
 
 void main()
 {
-	dirEntries("", SpanMode.shallow)
-		.filter!(de => de.isDir && (de.attributes & FILE_ATTRIBUTE_HIDDEN) == 0)
+	thisExePath
+		.dirName
+		.dirName
+		.buildPath("descript.ion")
+		.readText()
+		.splitLines()
+		.filter!(line => !line.canFind("[resolved:") && !line.canFind("[?]") && !line.canFind("[pull]"))
 		.array
 		[uniform(0, $)]
-		.I!(de => "https://issues.dlang.org/show_bug.cgi?id=" ~ de.name)
+		.I!(line => "https://issues.dlang.org/show_bug.cgi?id=" ~ line.split[0])
 		.browse();
 }
